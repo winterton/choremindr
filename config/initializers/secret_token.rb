dd5273ac2639a1224e93c18ee39969c49f6032fc
Jacob Smith <smithjw@agileoasis.com>
2013-04-02 08:16:38 -0400

    Initial skeleton for webapp. Further development will occur on the develop branch, master is reserved for production ready code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cm::Application.config.secret_token = '35bf9429f8f8d9bcb0de3a5fddc3f1df2917338c1bfba9f449d9f28b0fb3a4946649628e479581b6be15560f1c36d52fb9750b056be3e763f6018f5b100a8fd4'
